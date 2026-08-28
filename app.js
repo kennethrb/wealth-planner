d193144159335008839111dc53112c55d11f402f
kennethrb <81312961+kennethrb@users.noreply.github.com>
2026-08-29 04:15:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2181,3 +2181,46 @@ function setupScrollSpy() {
 
   sections.forEach((section) => observer.observe(section));
 }
+
+const backToTop =
+  document.getElementById(
+    "backToTop"
+  );
+
+window.addEventListener(
+  "scroll",
+  () => {
+
+    if (
+      window.scrollY > 500
+    ) {
+
+      backToTop.classList.add(
+        "show"
+      );
+
+    } else {
+
+      backToTop.classList.remove(
+        "show"
+      );
+
+    }
+
+  }
+);
+
+backToTop.addEventListener(
+  "click",
+  () => {
+
+    window.scrollTo({
+
+      top: 0,
+
+      behavior: "smooth"
+
+    });
+
+  }
+);
