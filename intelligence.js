551f48b70330f1cec1f108143ce5cbe71c733c6a
kennethrb <81312961+kennethrb@users.noreply.github.com>
2026-09-01 11:18:36 +0800

    Create intelligence.js

---
+++ intelligence.js
@@ -0,0 +1,11 @@
+/**
+ * Wealth Planner Intelligence Engine
+ *
+ * Roadmap Features
+ *
+ * 1. Buffer vs Invest
+ * 2. Net Worth Velocity
+ * 3. Personal Inflation Engine
+ * 4. Purchase Evaluator
+ * 5. Wealth Sweep Engine
+ */
