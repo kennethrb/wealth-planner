8b58121639a6a87490348256e801c6d98cf9abf3
kennethrb <81312961+kennethrb@users.noreply.github.com>
2026-09-13 15:01:55 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+    emergencyFundMonths: 6,
+    investmentReturnRate: 0.07,
+    wealthSweep: {
+        investment: 0.70,
+        debt: 0.20,
+        emergency: 0.10
+    },
+    opportunityAllocation: {
+        goals: 0.30,
+        investments: 0.50,
+        debt: 0.20
+    }
+};
