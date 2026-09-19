cd9019ab2a83235033592bd8991a8aaef25eb29c
kennethrb <81312961+kennethrb@users.noreply.github.com>
2026-09-20 00:28:49 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -429,38 +429,6 @@ const difference =
     currentBalance -
     expectedBalance;
 
-if (
-    account.accountName === "GoTrade" ||
-    account.accountName === "MP2"
-) {
-
-    console.group(
-        `RECON DEBUG - ${account.accountName}`
-    );
-
-    console.log(
-        "Opening Balance:",
-        openingBalance
-    );
-
-    console.log(
-        "Current Balance:",
-        currentBalance
-    );
-
-    console.log(
-        "Expected Balance:",
-        expectedBalance
-    );
-
-    console.log(
-        "Difference:",
-        difference
-    );
-
-    console.groupEnd();
-}
-
 const reconciled =
     Math.abs(difference) < 0.01;
         if (reconciled) {
