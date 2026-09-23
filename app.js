beb0697ac6e771a576410143c9e85093e893ef97
kennethrb <81312961+kennethrb@users.noreply.github.com>
2026-09-23 21:42:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -877,3 +877,18 @@ function showIntelTab(tab) {
         .getElementById(`btn-${tab}`)
         .classList.add("active");
 }
+
+function loadCategoryDropdown() {
+    const deleteSelect = document.getElementById("deleteCategorySelect");
+    if (deleteSelect) deleteSelect.innerHTML = "";
+    appData.categories.forEach(cat => {
+        if (deleteSelect) {
+            deleteSelect.innerHTML += `
+                <option value="${cat.categoryId}">
+                    ${cat.categoryName}
+                </option>
+            `;
+        }
+    });
+}
+let hasUnsavedBudgetChanges = false;
